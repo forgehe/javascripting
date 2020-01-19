2ac00cb75c329668eac741a0b3d1bdd1fadd8481
Richard Liao <liao098@gmail.com>
2020-01-19 23:29:02 +0000

    Finishing NodeSchool

---
+++ functions.js
@@ -0,0 +1,13 @@
+//define a function named eat that takes an argument 
+//named food that is expected to be a string. 
+
+function eat(food) {
+  return food + ' tasted really good.';
+}
+
+console.log(eat("bananas"));
+
+
+// Inside of the parentheses of console.log(), 
+// call the eat() function with  
+// the string bananas as the argument.
